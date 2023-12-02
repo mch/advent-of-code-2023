7c7d2641623f698a0bb3f69e571f040ab404d950
Matt Hughes <matt.c.hughes@gmail.com>
2023-12-02 13:07:02 -0700

    day 1 test 2

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -6,7 +6,8 @@ pub fn puzzle(input: &str) {
 }
 
 fn recover_calibration_value(line: &str) -> String {
-    "77".to_string()
+    let matches: Vec<&str> = line.matches(char::is_numeric).collect();
+    matches.join("")
 }
 
 #[cfg(test)]
@@ -19,10 +20,23 @@ mod tests {
     // a1b2c3d4e5f  15
     // treb7uchet   77
 
+    // Invariants
+    // Always return a string with two characters
+    // Both characters match [0-9]
+
     #[test]
     fn test_digits_at_start_and_end() {
         let line = "1abc2";
-        let expected = "77";
+        let expected = "12";
+
+        let result = recover_calibration_value(&line);
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn test_digits_in_middle() {
+        let line = "pqr3stu8vwx";
+        let expected = "38";
 
         let result = recover_calibration_value(&line);
         assert_eq!(expected, result);
